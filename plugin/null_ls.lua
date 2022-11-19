aecbaf81778cdaed4a8cb8b9e504a7c7cbdfd4ec
Tormod Fossum <51208715+torcor-dev@users.noreply.github.com>
2022-11-19 10:07:33 +0100

    Some changes (#5)
    Co-authored-by: Tormod Fossum <fossum@pm.me>

--- plugin/null_ls.lua
+++ plugin/null_ls.lua
@@ -1,14 +1,15 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.golines,
-    },
+
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		--null_ls.builtins.diagnostics.eslint,
+		--null_ls.builtins.code_actions.eslint,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.diagnostics.flake8,
+	},
 })
-
-
